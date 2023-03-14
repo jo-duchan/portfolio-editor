dccd916958f24a0f265834a9ebe77e9d720dd0f7
jo-duchan <jo_duchan@icloud.com>
2023-03-14 11:45:45 +0900

    DummyData 구조 설계

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,24 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "styles/common";
 
+const dummyData = [
+  {
+    id: "get0",
+    tag: "p",
+    content: "Create First Component",
+  },
+  {
+    id: "get1",
+    tag: "p",
+    content: "Create Second Component",
+  },
+  {
+    id: "get2",
+    tag: "p",
+    content: "Create Third Component",
+  },
+];
+
 function App() {
   return (
     <BrowserRouter>
