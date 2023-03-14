a94e6787c68d1f7b10a9e4fd321197036e437cf1
jo-duchan <jo_duchan@icloud.com>
2023-03-14 11:52:46 +0900

    Pages Home, Edit 생성 / Router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "styles/common";
 
+// Pages
+import Home from "pages/Home";
+import Edit from "pages/Edit";
+
 const dummyData = [
   {
     id: "get0",
@@ -25,7 +29,8 @@ function App() {
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<>APP</>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/edit" element={<Edit />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Edit.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function Edit() {
+  return <Container>Edit</Container>;
+}
+
+export default Edit;
+
+const Container = styled.div``;
