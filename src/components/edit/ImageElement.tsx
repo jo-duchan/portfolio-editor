94e908b4ba933dd57998bee28f670734b7e0e495
jo-duchan <jo_duchan@icloud.com>
2023-03-20 00:26:05 +0900

    ImageElement 생성

---
+++ src/components/edit/ImageElement.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+// Type
+import {} from "type/contentDataType";
+
+interface Props {}
+
+interface StyledProps {}
+
+function ImageElement({}: Props) {
+  return <Container />;
+}
+
+export default ImageElement;
+
+const Container = styled.div<StyledProps>``;
